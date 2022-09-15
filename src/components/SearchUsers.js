2cd8446ce224a6c5d3ee2c2fb62368f4e8f94155
Nathan <namo678@me.com>
2022-09-15 09:58:13 -0400

    search users

--- src/components/SearchUsers.js
+++ src/components/SearchUsers.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const SearchUsers = () => {
+const SearchUsers = ({userInfo}) => {
+    console.log(userInfo)
 
+    const [searchedNames, setSearchedNames] = useState([])
+    const [searchValue, setSearchValue] = useState('')
+
+    
+    const handleChange = (e) => {
+        setSearchValue(e.target.value.toLowerCase())
+        let newArr = userInfo.filter((user) => {
+            return user.name.toLowerCase().includes(searchValue)
+        })
+        setSearchedNames([...newArr])
+    }
+    
+    
+    
     return (
-        <h1>Search for Users</h1>
+        <div>
+        <h1>Search for Users {userInfo.length}</h1>
+        <form value={searchValue}>
+          <input  onChange={handleChange} />
+        </form>
+        {
+          userInfo.filter((p) => {
+            return p.name.toLowerCase().includes(searchValue)
+          }).map((user, i) => {
+            return (
+            <div key={i}>
+            <p style={{color: 'white'}} key={user.id}><Link to='/searched-profile'>{user.name}</Link></p>
+        </div>
+        )
+          })
+        }
+        </div>
     )
 
 }
